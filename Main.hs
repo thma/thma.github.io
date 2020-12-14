394c7bf523fbdce0eaada413e1f9e57d85fe0b1f
Mahler, Thomas <thomas.mahler@ista.com>
2020-12-14 11:26:07 +0100

    update link

--- Main.hs
+++ Main.hs
@@ -1,111 +1,113 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import           Data.Monoid (mappend)
-import           Hakyll
 import qualified GHC.IO.Encoding as E
-
-
+import Hakyll
 
 main :: IO ()
 main = do
   E.setLocaleEncoding E.utf8
   hakyllWith config $ do
     match "static/*/*" $ do
-        route idRoute
-        compile copyFileCompiler
-  
+      route idRoute
+      compile copyFileCompiler
+
     match "img/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route idRoute
+      compile copyFileCompiler
 
     match "favicon/*" $ do
-        route $ customRoute $ drop 8 . toFilePath
-        compile copyFileCompiler
+      route $ customRoute $ drop 8 . toFilePath
+      compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route idRoute
+      compile compressCssCompiler
 
     match (fromList ["about.md", "contact.md"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $
+        pandocCompiler
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $
+        pandocCompiler
+          >>= loadAndApplyTemplate "templates/post.html" postCtx
+          >>= saveSnapshot "content"
+          >>= loadAndApplyTemplate "templates/default.html" postCtx
+          >>= relativizeUrls
 
     create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let archiveCtx =
+              listField "posts" postCtx (return posts)
+                `mappend` constField "title" "Archives"
+                `mappend` defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+          >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+          >>= relativizeUrls
 
     create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend` bodyField "description"
-            posts <- fmap (take 10) . recentFirst =<<
-                loadAllSnapshots "posts/*" "content"
-            renderAtom myFeedConfiguration feedCtx posts
+      route idRoute
+      compile $ do
+        let feedCtx = postCtx `mappend` bodyField "description"
+        posts <-
+          fmap (take 10) . recentFirst
+            =<< loadAllSnapshots "posts/*" "content"
+        renderAtom myFeedConfiguration feedCtx posts
 
     create ["rss.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend` bodyField "description"
-            posts <- fmap (take 10) . recentFirst =<<
-                loadAllSnapshots "posts/*" "content"
-            renderRss myFeedConfiguration feedCtx posts
-
+      route idRoute
+      compile $ do
+        let feedCtx = postCtx `mappend` bodyField "description"
+        posts <-
+          fmap (take 10) . recentFirst
+            =<< loadAllSnapshots "posts/*" "content"
+        renderRss myFeedConfiguration feedCtx posts
 
     match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let indexCtx =
+              listField "posts" postCtx (return posts)
+                `mappend` constField "title" "Home"
+                `mappend` defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+        getResourceBody
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
 
 config :: Configuration
-config = defaultConfiguration
-    { destinationDirectory = "docs"
-    , deployCommand = "publish.sh"
+config =
+  defaultConfiguration
+    { destinationDirectory = "docs",
+      deployCommand = "publish.sh"
     }
 
 myFeedConfiguration :: FeedConfiguration
-myFeedConfiguration = FeedConfiguration
-    { feedTitle       = "LambdaBlog: lambda is not a four letter word"
-    , feedDescription = "This feed lists the latest blog posts"
-    , feedAuthorName  = "Thomas Mahler"
-    , feedAuthorEmail = "Thomas.Mahler@ista.com"
-    , feedRoot        = "https://thma.github.io/LambdaBlog/"
+myFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "LambdaBlog: lambda is not a four letter word",
+      feedDescription = "This feed lists the latest blog posts",
+      feedAuthorName = "Thomas Mahler",
+      feedAuthorEmail = "Thomas.Mahler@ista.com",
+      feedRoot = "https://thma.github.io/"
     }
